c575abdefa081d85d714906f61b7be10692874b6
Raul Prado Dantas <82855362+raulzera99@users.noreply.github.com>
2022-07-31 22:33:30 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -2,18 +2,25 @@ package gui;
 
 import java.awt.Label;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -21,6 +28,8 @@ public class DepartmentFormController implements Initializable {
 	private DepartmentService service;
 
 	private Department entity;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 
 	@FXML
 	private TextField txtId;
@@ -45,6 +54,10 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if(entity == null) {
@@ -55,13 +68,24 @@ public class DepartmentFormController implements Initializable {
 			throw new IllegalStateException("Service was null");  
 		}
 		try {
-			entity.getFormData();
+			entity = getFormData();
 			service.saveOrUpdate(entity);
-			Utils.currentStage(event).close();
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close(); 
 			
-		}catch(DbException e) {
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener: dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
 	}
 
 	private Department getFormData() {
@@ -84,8 +108,8 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	@FXML
-	public void onBtCancelAction() {
-
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close(); 
 	}
 
 	@Override
@@ -104,6 +128,15 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> error) {
+		Set<String> fields = error.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(error.get("name"));
+		}
+		
 	}
 
 }
